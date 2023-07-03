c7efdfa3ebcd5a9f80c20d98c02addfc4d80096c
Sreesanth46 <sreesanth46@gmail.com>
2023-07-03 22:42:52 +0530

    chore: mongodb connection, routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import router from "./router/index.js";
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 const app: Express = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(" Hello World");
-})
+app.use(bodyParser.json());
+app.use('/', router());
+
+mongoose.connect('mongodb://localhost:27017', {})
+    .then(() => console.log("DB connected"))
+    .catch((err: Error) => console.log("Error connecting to MongoDB", err))
 
 app.listen(port, () => {
     console.log(`Server listening on ${port}`);
